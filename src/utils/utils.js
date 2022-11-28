f4e10614344f5381c8eb3fee990eca1695d144ed
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-11-28 16:37:17 +0800

    feat:获取日期新增年、月返回

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,6 +6,14 @@
 export const getTimeInterval = (time) => {
   const now = new Date().getTime()
   const timeDifference = (now - time) / 1000
+  const year =
+    Math.floor(timeDifference / (60 * 60 * 24 * 30 * 365)) > 0
+      ? `${Math.floor(timeDifference / (60 * 60 * 24 * 30 * 365))} 年前`
+      : null
+  const month =
+    Math.floor(timeDifference / (60 * 60 * 24 * 30)) > 0
+      ? `${Math.floor(timeDifference / (60 * 60 * 24 * 30))} 个月前`
+      : null
   const day =
     Math.floor(timeDifference / (60 * 60 * 24)) > 0
       ? `${Math.floor(timeDifference / (60 * 60 * 24))} 天前`
@@ -19,5 +27,5 @@ export const getTimeInterval = (time) => {
       ? `${Math.floor(timeDifference / 60)} 分钟前`
       : null
   const second = `${Math.floor(timeDifference)} 秒前`
-  return day ?? hour ?? min ?? second
+  return year ?? month ?? day ?? hour ?? min ?? second
 }
