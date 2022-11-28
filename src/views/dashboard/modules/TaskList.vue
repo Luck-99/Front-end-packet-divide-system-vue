765a1c7c3d7fb4eee29b10e8ed3c7e1f8b6e9e81
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-11-28 11:15:58 +0800

    refactor:删除冗余任务列表组件，改为table组件

--- src/views/dashboard/modules/TaskList.vue
+++ src/views/dashboard/modules/TaskList.vue
@@ -1,38 +1,77 @@
+<template>
+  <div class="dash-borard-task-list">
+    <div class="topTitle borderBottom">{{ title }}</div>
+    <el-table :data="allJobs" :show-header="false">
+      <el-table-column prop="description" label="描述"> </el-table-column>
+      <el-table-column prop="fullName" label="全称"> </el-table-column>
+      <el-table-column prop="lastBuild.timestamp" label="时间">
+        <template slot-scope="scope">
+          {{ getTime(scope.row.lastBuild.timestamp) }}
+        </template> </el-table-column
+      ><el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button
+            icon="el-icon-video-play"
+            circle
+            @click="handleRowClick(scope.row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
 <script>
-import TaskItem from "../components/TaskItem.vue"
+import { getAllJob } from "@/services/jenkins"
 export default {
-  components: { TaskItem },
+  components: {},
   name: "TaskList",
   data() {
     return {
       title: "任务列表",
+      allJobs: [],
     }
   },
-  methods: {},
+  methods: {
+    getAllJenkinsJob: async function () {
+      const res = await getAllJob()
+      if (res.code > 0) {
+        this.allJobs = res.data
+      }
+    },
+    handleRowClick: function (params) {
+      console.log(params)
+    },
+    getTime: function (time) {
+      const now = new Date().getTime()
+      const timeDifference = (now - time) / 1000
+      const day =
+        Math.floor(timeDifference / (60 * 60 * 24)) > 0
+          ? `${Math.floor(timeDifference / (60 * 60 * 24))} 天前`
+          : null
+      const hour =
+        Math.floor(timeDifference / (60 * 60)) > 0
+          ? `${Math.floor(timeDifference / (60 * 60))} 小时前`
+          : null
+      const min =
+        Math.floor(timeDifference / 60) > 0
+          ? `${Math.floor(timeDifference / 60)} 分钟前`
+          : null
+      const second =
+        Math.floor(timeDifference) > 0
+          ? `${Math.floor(timeDifference)} 秒前`
+          : null
+      return day ?? hour ?? min ?? second
+    },
+  },
+  mounted() {
+    this.getAllJenkinsJob()
+  },
 }
 </script>
 
-<template>
-  <div class="taskListmain">
-    <div class="topTitle borderBottom">{{ title }}</div>
-    <div>
-      <task-item
-        :title="'太保环境'"
-        :userName="'小张'"
-        :lastStatus="'5小时前'"
-      />
-      <task-item
-        :title="'121环境'"
-        :userName="'小许'"
-        :lastStatus="'13小时前'"
-      />
-      <task-item :title="'南银环境'" :userName="'小张'" :lastStatus="'2天前'" />
-    </div>
-  </div>
-</template>
-
 <style lang="less" scoped>
-.taskListmain {
+.dash-borard-task-list {
   background-color: #fff;
   .topTitle {
     text-align: left;
