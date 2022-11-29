4cdda4ba91941c9dad8cce537d1c4e4fe6efdbe2
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-11-29 15:59:08 +0800

    doc：新增接口注释

--- src/services/jenkins.ts
+++ src/services/jenkins.ts
@@ -1,5 +1,10 @@
 import { request } from "@/utils/axios"
 
+/**
+ * 获取所有任务
+ * @param params:{tree} 用于筛选 例：jobs[buildable,lastBuild[timestamp,result]]
+ * @returns 所有任务信息
+ */
 export async function getAllJobs(params: { tree: string }): Promise<any> {
   return request("/jenkins/getAllJobs", {
     method: "GET",
@@ -7,6 +12,11 @@ export async function getAllJobs(params: { tree: string }): Promise<any> {
   })
 }
 
+/**
+ * 构建任务
+ * @param params:{name} 构建任务 传入任务名称
+ * @returns 构建成功信息
+ */
 export async function buildJob(params: { name: string }): Promise<any> {
   return request("/jenkins/buildJob", {
     method: "GET",
@@ -14,6 +24,11 @@ export async function buildJob(params: { name: string }): Promise<any> {
   })
 }
 
+/**
+ * 获取任务详情
+ * @param params {name：任务名称，tree：用于筛选}
+ * @returns 任务详情
+ */
 export async function getJobInfo(params: {
   name: string
   tree: string
