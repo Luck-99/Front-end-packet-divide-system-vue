8dbd9fbbb8f562338042ee85a7a11ce58ee57be8
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-05 17:50:33 +0800

    feat:vuesocket通信

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import Element from "element-ui"
+import VueSocketio from "vue-socket.io"
+
 import "element-ui/lib/theme-chalk/index.css"
 
 import "@/assets/css/global.less"
@@ -11,8 +13,23 @@ Vue.config.productionTip = false
 
 Vue.use(Element, { size: "small", zIndex: 3000 })
 
+Vue.use(
+  new VueSocketio({
+    debug: true,
+    connection: "http://127.0.0.1:7002/",
+  })
+)
+
 new Vue({
   router,
   store,
+  sockets: {
+    connect: function () {
+      console.log("连接上")
+    },
+    res: function (val) {
+      console.log("接收到", val)
+    },
+  },
   render: (h) => h(App),
 }).$mount("#app")
