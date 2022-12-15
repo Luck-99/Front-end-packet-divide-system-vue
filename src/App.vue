276a9672852e3ce4714378a9e2215a012bc87a78
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-15 11:17:27 +0800

    feat:滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,37 @@ export default {
   color: #2c3e50;
   height: 100%;
   font-size: 16px;
+
+  /*滚动条整体粗细样式*/
+  ::-webkit-scrollbar {
+    /*高宽分别对应横竖滚动条的尺寸*/
+    width: 8px;
+    height: 8px;
+  } /*滚动条里面小方块*/
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px !important;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2) !important;
+    /* 颜色 */
+    background: #b6b6b6 !important;
+    /* 线性渐变背景 */
+    // background-image: linear-gradient(
+    //   45deg,
+    //   #ffbd61 25%,
+    //   #ffbd61 25%,
+    //   #ff8800 25%,
+    //   #ff8800 50%,
+    //   #ffbd61 50%,
+    //   #ffbd61 75%,
+    //   #ff8800 75%,
+    //   #ff8800 100%
+    // ) !important;
+  }
+  /*滚动条轨道*/
+  ::-webkit-scrollbar-track {
+    border-radius: 10px !important;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2) !important;
+    background: #ededed !important;
+  }
 }
 
 nav {
