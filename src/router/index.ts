b7839cea858455d40466b7055ee878a38b116812
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-16 17:00:00 +0800

    refactor:路由守卫测试

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
+    path: "/login",
     name: "login",
     component: () => import("@/views/user/LoginView.vue"),
   },
@@ -20,7 +20,7 @@ const routes: Array<RouteConfig> = [
     component: () => import("@/views/taskSetting/TaskSetting.vue"),
   },
   {
-    path: "/home",
+    path: "/",
     name: "home",
     component: () => import("@/views/HomeView.vue"),
   },
@@ -37,6 +37,23 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  // to:即将到达的路由    from:即将离开的路由 next:向下执行的方法
+  if (to.path == "/login") {
+    // 如果是登录页面,放行
+    next()
+  } else {
+    // 如果不是登录页面,验证token。如果有token,则执行next(),否则跳转到登录页面
+    const token = true //getCookie("token")
+    if (token) {
+      next()
+    }
+    if (!token) {
+      next("/login")
+    }
+  }
 })
 
 export default router
