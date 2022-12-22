07cbfcff09736161164f4936f064bfbbd232518c
zhangzelin <zhangzelin@apexsoft.com.cn>
2022-12-22 11:46:59 +0800

    refactor:删除冗余代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,17 +22,8 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "home",
-    component: () => import("@/views/HomeView.vue"),
+    // component: () => import("@/views/HomeView.vue"),
     redirect: "/dashBoard",
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
   },
 ]
 
