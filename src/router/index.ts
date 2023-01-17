a2913432c26eb07ccaf12bfb64b227055155242f
zhangzelin <zhangzelin@apexsoft.com.cn>
2023-01-17 13:38:33 +0800

    refactor:空白路由重定向到首页

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,6 +13,7 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "home",
     component: () => import("@/views/main/main.vue"),
+    redirect: "dashBoard",
     children: [
       {
         path: "dashBoard",
@@ -32,21 +33,21 @@ const router = new VueRouter({
   routes,
 })
 
-router.beforeEach((to, from, next) => {
-  // to:即将到达的路由    from:即将离开的路由 next:向下执行的方法
-  if (to.path == "/login") {
-    // 如果是登录页面,放行
-    next()
-  } else {
-    // 如果不是登录页面,验证token。如果有token,则执行next(),否则跳转到登录页面
-    const token = true //getCookie("token")
-    if (token) {
-      next()
-    }
-    if (!token) {
-      next("/login")
-    }
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   // to:即将到达的路由    from:即将离开的路由 next:向下执行的方法
+//   if (to.path == "/login") {
+//     // 如果是登录页面,放行
+//     next()
+//   } else {
+//     // 如果不是登录页面,验证token。如果有token,则执行next(),否则跳转到登录页面
+//     const token = true //getCookie("token")
+//     if (token) {
+//       next()
+//     }
+//     if (!token) {
+//       next("/login")
+//     }
+//   }
+// })
 
 export default router
